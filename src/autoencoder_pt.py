f4d86c6cd585da5603c9230152f4c5f63c1e5766
Prannav <parora9@wisc.edu>
2021-06-15 12:20:37 -0500

    Added a train_model function

--- src/autoencoder_pt.py
+++ src/autoencoder_pt.py
@@ -3,6 +3,7 @@ from typing import Tuple
 from torchinfo import summary
 import data, config
 from torch.utils.data import DataLoader
+from sklearn.metrics import mean_squared_error
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('Using {} device'.format(device))
@@ -16,7 +17,8 @@ class Autoencoder_PT(torch.nn.Module):
         decoder_hidden_layers: Tuple, 
         relu_negative_slope: float = 0.0,
         model_input_shape: Tuple = (4,1,8,8,8),
-        signal_window_size: int = 8):
+        signal_window_size: int = 8,
+        learning_rate: float = .01):
 
         super(Autoencoder_PT, self).__init__()
 
@@ -39,6 +41,9 @@ class Autoencoder_PT(torch.nn.Module):
         self.create_encoder()
         self.decoder = torch.nn.Sequential()
         self.create_decoder()
+
+        self.loss = torch.nn.MSELoss()
+        self.optimizer = torch.optim.SGD(self.parameters(), lr=learning_rate, momentum=0.9)
 
         return
 
@@ -83,7 +88,41 @@ class Autoencoder_PT(torch.nn.Module):
         decoded = self.decoder(encoded)
         return decoded.view(self.model_input_shape)
 
+    # Train this autoencoder model
+    def train_model(self, dataloader: DataLoader, epochs: int = 3, print_output: bool = True):
+        if print_output:
+            print('Beginning Training Model')
 
+        # loop over the dataset multiple times
+        for epoch in range(epochs):  
+            running_loss = 0.0
+            for i, data in enumerate(dataloader):
+                # get the inputs; data is a list of [inputs, labels]
+                inputs, labels = data
+
+                # zero the parameter gradients
+                self.optimizer.zero_grad()
+
+                # forward + backward + optimize
+                outputs = self(inputs)
+                loss = self.loss(outputs, labels)
+                loss.backward()
+                self.optimizer.step()
+
+                # print statistics
+                running_loss += loss.item()
+
+                if(print_output):
+                    # print every 2000 mini-batches
+                    if i % 2000 == 1999:    
+                        print('Epoch %d, loss after sample #%5d: %.3f' %
+                              (epoch + 1, i + 1, running_loss / 2000))
+                        running_loss = 0.0
+
+        
+        if(print_output):
+            print('Finished Training')
+        return
 
 if __name__== '__main__':
     model = Autoencoder_PT(32, 
@@ -95,7 +134,7 @@ if __name__== '__main__':
 
     fold = 1
     data_ = data.Data(kfold=True, balance_classes=config.balance_classes)
-    train_data, _, _ = data_.get_data(shuffle_sample_indices=True, fold=fold)
+    train_data, test_data, _ = data_.get_data(shuffle_sample_indices=True, fold=fold)
     
     train_dataset = data.ELMDataset(
         *train_data,
@@ -103,17 +142,27 @@ if __name__== '__main__':
         config.label_look_ahead,
         stack_elm_events=False,
         transform=None,
+        for_autoencoder = True
     )
 
-    sample = train_dataset.__getitem__(0)
-    # print(sample[1])
-    # print(sample[0].shape)
-    # print(sample[0])
+    test_dataset = data.ELMDataset(
+        *test_data,
+        config.signal_window_size,
+        config.label_look_ahead,
+        stack_elm_events=False,
+        transform=None,
+        for_autoencoder = True
+    )
 
     train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True)
-    train_features, train_labels = next(iter(train_dataloader))
-    print(train_features.size())
-    print(train_labels.size())
+    test_dataloader = DataLoader(test_dataset, batch_size=4, shuffle=True)
 
+    # Save the model
+    model.train_model(train_dataloader, epochs = 1)
+
+    # Save the model
+    model_save_path = './models/trained_model.pth'
+    torch.save(model.state_dict(), model_save_path)
+    
         
 
