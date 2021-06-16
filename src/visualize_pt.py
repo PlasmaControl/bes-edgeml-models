f8edd600364d8baeaeb8907395d72a9da7507340
Prannav <parora9@wisc.edu>
2021-06-15 21:10:25 -0500

    Animation displays a chose ELM event now

--- src/visualize_pt.py
+++ src/visualize_pt.py
@@ -14,34 +14,43 @@ train_data, _, _ = data_.get_data(shuffle_sample_indices=False, fold=fold)
 signal_frames = train_data[0]
 signal_labels = train_data[1]
 
+
+s = signal_frames[15000:15300,...]
+l = signal_labels[15000:15300,...]
+
+s = signal_frames[20610:21000,...]
+l = signal_labels[20610:21000,...]
+
 fig = plt.figure()
-# data = np.random.rand(8, 8)
-# sb.heatmap(data, square=True)
+data = np.random.rand(8, 8)
+sb.heatmap(data, square=True)
 # print(signal_frames[0])
 
 def init():
-    sb.heatmap(np.zeros((8, 8)), vmin = -1, vmax = 1, square=True, cbar=False, xticklabels=False, yticklabels=False, cmap = 'hot')
+    sb.heatmap(np.zeros((8, 8)), square=True, cbar=True, xticklabels=False, yticklabels=False, cmap = 'hot')
 
 def animate(i):
     plt.clf()
-    data = signal_frames[i]
-    sb.heatmap(data, square=True, vmin = -1, vmax = 1, cbar=True, xticklabels=False, yticklabels=False, cmap = 'hot')
+    # print(f'Label at frame {i}: {l[i]}')
+    data = s[i]
+    label = l[i]
+    plt.title(f'Label: {label}')
+    sb.heatmap(data, vmin = -1, vmax = 1, square=True, cbar=True, xticklabels=False, yticklabels=False, cmap = 'hot')
 
 
-# def init():
-#     plt.imshow(np.zeros((8, 8)), cmap = 'hot')
-
-# def animate(i):
-#     plt.clf()
-#     print(f'Label at frame {i}: {signal_labels[i]}')
-#     data = signal_frames[i]
-#     plt.imshow(data, cmap = 'hot')
+def show_animation():
+    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=len(s), interval = 10, repeat = False)
+    plt.show()
 
 
-# anim = animation.FuncAnimation(fig, animate, init_func=init, frames=len(signal_frames), interval = 1, repeat = False)
-# plt.show()
+if __name__ == '__main__':
+    show_animation()
 
+    for i, label in enumerate(signal_labels):
+        if label == 1.0:
+            print(i, label)
 
-# for i, label in enumerate(signal_labels):
-#     if label == 1.0:
-#         print(i, label)
+    max_val = np.amax(signal_frames)
+    min_val = np.amin(signal_frames)
+
+    # print(f'Max: {max_val}, Min: {min_val}')
