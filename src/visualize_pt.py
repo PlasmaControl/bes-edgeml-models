ce068a161ad68c8761923d7382117ce4a9009e46
Prannav <parora9@wisc.edu>
2021-06-21 09:17:54 -0500

    Displays actual vs prediction frames

--- src/visualize_pt.py
+++ src/visualize_pt.py
@@ -1,14 +1,60 @@
+import torch
+# from autoencoder_pt import Autoencoder_PT
+from ae_easy import Autoencoder_easy
 import data, config
 from matplotlib import pyplot as plt
-from matplotlib import animation
+from matplotlib.animation import FuncAnimation, FFMpegWriter
 import seaborn as sb
 import numpy as np
+from scipy.ndimage.filters import gaussian_filter
 
 fold = 1
-
 data_ = data.Data(kfold=True, balance_classes=config.balance_classes)
-train_data, _, _ = data_.get_data(shuffle_sample_indices=False, fold=fold)
+train_data, test_data, _ = data_.get_data(shuffle_sample_indices=False, fold=fold)
 
+train_dataset = data.ELMDataset(
+        *train_data,
+        config.signal_window_size,
+        config.label_look_ahead,
+        stack_elm_events=False,
+        transform=None,
+        for_autoencoder = True
+    )
+
+# Load model
+PATH = 'models/easy_model.pth'
+model = torch.load(PATH)
+model.eval()
+
+
+def plot(index):
+    actual_window = train_dataset[index][0]
+    pred_window = train_dataset[index][1]
+    model_window = model(actual_window)
+    
+    number_frames = 4
+    number_rows = 2
+    fig, ax = plt.subplots(nrows = number_rows, ncols = number_frames)
+
+    # Plot the actual frames 0,2,4,6
+    actual = actual_window.detach().numpy()[0]
+    for i in range(number_frames):
+        cur_ax = ax[0][i]
+        cur_ax.imshow(actual[2*i], cmap = 'hot')
+        cur_ax.set_title(f'A {2*i}')
+        cur_ax.axis('off')
+
+    # Plot the prediction frames 0,2,4,6
+    pred = model_window.detach().numpy()[0]
+    for i in range(number_frames):
+        cur_ax = ax[1][i]
+        cur_ax.imshow(pred[2*i], cmap = 'hot')
+        cur_ax.set_title(f'P {2*i}')
+        cur_ax.axis('off')
+
+    fig.tight_layout()
+    # fig.savefig('plot.png') 
+    plt.show()
 
 # train_data[0] is a numpy array of many 8x8 signal frames
 signal_frames = train_data[0]
@@ -18,13 +64,7 @@ signal_labels = train_data[1]
 s = signal_frames[15000:15300,...]
 l = signal_labels[15000:15300,...]
 
-s = signal_frames[20610:21000,...]
-l = signal_labels[20610:21000,...]
-
-fig = plt.figure()
-data = np.random.rand(8, 8)
-sb.heatmap(data, square=True)
-# print(signal_frames[0])
+# fig = plt.figure()
 
 def init():
     sb.heatmap(np.zeros((8, 8)), square=True, cbar=True, xticklabels=False, yticklabels=False, cmap = 'hot')
@@ -34,23 +74,27 @@ def animate(i):
     # print(f'Label at frame {i}: {l[i]}')
     data = s[i]
     label = l[i]
+    # print(i)
     plt.title(f'Label: {label}')
     sb.heatmap(data, vmin = -1, vmax = 1, square=True, cbar=True, xticklabels=False, yticklabels=False, cmap = 'hot')
 
 
 def show_animation():
-    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=len(s), interval = 10, repeat = False)
+    anim = FuncAnimation(fig, animate, init_func=init, frames=len(s), interval = 1, repeat = False)
     plt.show()
 
 
+# def save_animation(save_path):
+#     anim = FuncAnimation(fig, animate, init_func=init, frames=len(s), interval = 1, repeat = False)
+#     writer = FFMpegWriter(fps=60, metadata=dict(artist='PA'), bitrate=1800)
+#     anim.save(save_path)
+#     plt.close()
+
+
 if __name__ == '__main__':
-    show_animation()
-
-    for i, label in enumerate(signal_labels):
-        if label == 1.0:
-            print(i, label)
-
-    max_val = np.amax(signal_frames)
-    min_val = np.amin(signal_frames)
-
-    # print(f'Max: {max_val}, Min: {min_val}')
+    for i in range(0, 11000, 1000):
+        # print(i)
+        plot(i)
+    
+    # show_animation()
+    # save_animation('elm_event_animation')
