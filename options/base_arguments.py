f7e65d2a3bb6b9361a7e8f947811d35be4480bd2
LakshyaMalhotra <lakshya9009@gmail.com>
2021-06-23 17:08:17 -0700

    Fixed base arguments.

--- options/base_arguments.py
+++ options/base_arguments.py
@@ -17,115 +17,112 @@ class BaseArguments:
         """Define the options common for training and testing."""
         # basic parameters
         parser.add_argument(
-            "--input_file", required=True, help="path to the input hdf5 file"
+            "--input_file", required=True, help="path to the input hdf5 file."
         )
         parser.add_argument(
             "--model_name",
             type=str,
             required=True,
-            help="name of the model to be used for training."
-            "[FeatureModel | CNNModel | ...]",
+            help="name of the model to be used for training, "
+            "[FeatureModel | CNNModel | ...].",
         )
         parser.add_argument(
             "--n_epochs",
             type=int,
             default=10,
-            help="total number of epochs for training. Default: 10",
+            help="total number of epochs for training.",
         )
         parser.add_argument(
             "--device",
             type=str,
             default="cpu",
-            help="device to be used for training and testing. [cpu | cuda], Default: cpu",
+            help="device to be used for training and testing, [cpu | cuda].",
         )
         parser.add_argument(
             "--k_fold",
             action="store_false",
-            help="if true, use K-fold cross-validation other makes standard train-test split."
-            "Default: False",
+            help="if true, use K-fold cross-validation other makes standard train-test split.",
         )
         parser.add_argument(
             "--max_elms",
             type=int,
             default=-1,
-            help="total number of elm events to be used. Use -1 to use all of them. Default: -1",
+            help="total number of elm events to be used. Use -1 to use all of them.",
         )
         # dataset parameters
         parser.add_argument(
             "--batch_size",
             type=int,
             default=64,
-            help="batch size for model training and testing. Default: 64",
+            help="batch size for model training and testing.",
         )
         parser.add_argument(
             "--signal_window_size",
             type=int,
             default=8,
-            help="number of time data points to use for the input."
-            "The size of each input will then become `signal_window_size x spatial_dims x spatial_dims`"
-            "[8 | 16]. Default: 8",
+            help="number of time data points to use for the input. "
+            "The size of each input will then become `signal_window_size x spatial_dims x spatial_dims`, "
+            "[8 | 16].",
         )
         parser.add_argument(
             "--label_look_ahead",
             type=int,
             default=0,
-            help="'look ahead', meaning the label for the entire signal window is taken to"
-            "be label corresponding to the last element (0 ahead) of the signal window."
-            "[0 | 4 | 8 | ...]. Default: 0",
+            help="`look ahead`, meaning the label for the entire signal window is taken to "
+            "be label corresponding to the last element (0 ahead) of the signal window, "
+            "[0 | 4 | 8 | ...].",
         )
         parser.add_argument(
             "--fraction_valid",
             type=float,
             default=0.2,
-            help="size of the validation dataset. Default: 0.2",
+            help="size of the validation dataset.",
         )
         parser.add_argument(
             "--fraction_test",
             type=float,
             default=0.1,
-            help="size of the test dataset. Default: 0.1",
+            help="size of the test dataset.",
         )
         parser.add_argument(
             "--size",
             type=int,
             default=8,
-            help="size of the input. Must be specified when using stacked ELM model."
-            "Default: 8",
+            help="size of the input. Must be specified when using stacked ELM model.",
         )
         parser.add_argument(
             "--data_mode",
             type=str,
             default="unbalanced",
-            help="which data mode to use, 'balanced' upsamples the data to reduce class imbalance"
-            "'unbalanced' uses the raw data as is. Default: unbalanced",
+            help="which data mode to use, `balanced` upsamples the data to reduce class imbalance "
+            "`unbalanced` uses the raw data as is.",
         )
         parser.add_argument(
             "--num_workers",
             type=int,
             default=0,
-            help="turns on the multi-processing data loading with `num_workers` loader"
-            "worker processes. Default: 0 meaning the data loading step will be done by"
+            help="turns on the multi-processing data loading with `num_workers` loader "
+            "worker processes. Default: 0 meaning the data loading step will be done by "
             "the main process.",
         )
         parser.add_argument(
             "--add_noise",
             action="store_false",
-            help="if true, add Gaussian noise to the data with user supplied mean and std dev"
-            "Default: False",
+            help="if true, add Gaussian noise to the data with user supplied mean and std dev.",
         )
         parser.add_argument(
             "--mu",
             type=float,
             default=0,
-            help="mean of the Gaussian noise. Must be passed"
-            "when '--add_noise=True'. Default: 0",
+            help="mean of the Gaussian noise. Must be passed "
+            "when `add_noise=True`.",
         )
         parser.add_argument(
             "--sigma",
             type=float,
             default=0.01,
-            help="standard deviation of the Gaussian noise. Must be passed"
-            "when '--add_noise=True'. Default: 0.01",
+            help="standard deviation of the Gaussian noise. Must be passed "
+            "when `add_noise=True`.",
         )
         self.initialized = True
 
