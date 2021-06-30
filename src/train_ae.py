64c547ab43b04d67b7e49a664cef74666a07b069
Prannav <parora9@wisc.edu>
2021-06-30 10:50:30 -0500

    automated training process

--- src/train_ae.py
+++ src/train_ae.py
@@ -3,13 +3,32 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
 import pickle
+import os
 from matplotlib import pyplot as plt
 from autoencoder import AE_simple, device
 import numpy as np
 
+from itertools import product
+from collections import namedtuple
+from collections import OrderedDict
+
 import config, data
 
 # ------------------------------------------------------------------------------------------------
+
+class RunBuilder():
+    @staticmethod
+    def get_runs(params):
+        Run = namedtuple('Run', params.keys())
+
+        runs = []
+
+        for v in product(*params.values()):
+            run = Run(*v)
+            # print(run)
+            runs.append(run)
+
+        return runs
 
 # Train function - this trains the passed in model by cycling 
 # between the train_loop() and validation_loop()
@@ -18,13 +37,14 @@ def train(model: AE_simple,
     test_dataloader: DataLoader, 
     optimizer,
     scheduler, 
-    loss_fn, 
+    loss_fn,
+    folder_name: str, 
     epochs: int = config.epochs,
     print_output: bool = True):
     
     run_name = model.latent_dim
 
-    tb = SummaryWriter(log_dir = f'runs/one_layer_{run_name}')
+    tb = SummaryWriter(log_dir = f'runs/{folder_name}/{run_name}')
 
     epoch_avg_losses = []
     all_losses = None
@@ -106,62 +126,13 @@ def validation_loop(model, dataloader: DataLoader, loss_fn, all_losses: bool = F
     else:
         return test_loss
 
-def plot(avg_losses, all_losses):
-    fig, (ax_l, ax_r) = plt.subplots(1,2) 
-    fig.set_size_inches(15, 5)
-    
-    # Left box - avg losses
-    ax_l.plot(np.arange(1, len(avg_losses)+1), avg_losses, linestyle='-', marker='o', color='b')
-    # ax_l.xticks(np.arange(1, len(avg_losses) + 1, 1.0))
-    ax_l.set_title('Test Loss vs. Epochs')
-    ax_l.set_ylabel('Avg Test Loss')
-    ax_l.set_xlabel('Epochs')
+def save_test_dataset(test_data, folder, filename):
+    parent_path = f'test_datasets/{folder}'
+    os.makedirs(parent_path, exist_ok = True)
+    filepath = parent_path + f'/{filename}'
 
-    # Right box - all Losses histogram
-    # ax_r.set_xlim(0,.5)
-    num_bins = 20
-    ax_r.hist(all_losses, edgecolor = 'black', bins = num_bins, log = True)
-
-    ax_r.set_xticks(np.linspace(np.amin(all_losses), np.amax(all_losses), num = num_bins, endpoint = True))
-    ax_r.set_xticklabels(ax_r.get_xticks(), rotation = 90)
-
-    ax_r.set_title('Last Epoch - All Losses histogram')
-    ax_r.set_ylabel('Batch Count')
-    ax_r.set_xlabel('Loss bins')
-    
-    plt.tight_layout()
-    plt.show()
-    # plt.savefig('./plots/loss_plot.png')
-
-if __name__ == '__main__':
-    # The model
-    model = AE_simple(300)
-    model = model.to(device)
-
-    loss_fn = torch.nn.MSELoss()
-
-    optimizer = torch.optim.SGD(
-        model.parameters(), 
-        lr=config.learning_rate, 
-        momentum=0.9, 
-        weight_decay=config.l2_factor)
-
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-            optimizer,
-            mode="min",
-            factor=0.5,
-            patience=2,
-            verbose=True,
-            eps=1e-6,
-        )    
-
-    # Get datasets and form dataloaders
-    data_ = data.Data(kfold=False, balance_classes=config.balance_classes)
-    train_data, valid_data, test_data = data_.get_data(shuffle_sample_indices=True) 
-
-    test_data_file = 'test_dataset'
     # dump test data into to a file
-    with open(test_data_file, "wb") as f:
+    with open(filepath, "wb") as f:
         pickle.dump(
             {
                 "signals": test_data[0],
@@ -171,38 +142,83 @@ if __name__ == '__main__':
             },
             f,
         )
+    return
 
-    train_dataset = data.ELMDataset(
-        *train_data,
-        config.signal_window_size,
-        config.label_look_ahead,
-        stack_elm_events=False,
-        transform=None,
-        for_autoencoder = True
-    )
+def save_model(model, folder, model_name):
+    parent_path = f'trained_models/{folder}'
+    os.makedirs(parent_path, exist_ok = True)
+    model_save_path = parent_path + f'/{model_name}'
 
-    valid_dataset = data.ELMDataset(
-        *valid_data,
-        config.signal_window_size,
-        config.label_look_ahead,
-        stack_elm_events=False,
-        transform=None,
-        for_autoencoder = True
-    )
-
-    batch_size = config.batch_size
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    valid_dataloader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
-
-    epoch_avg_losses, all_losses = train(model, 
-        train_dataloader, 
-        valid_dataloader, 
-        optimizer, 
-        scheduler, 
-        loss_fn)
-
-    # plot(epoch_avg_losses, all_losses)
-
-    # Save the model - weights and structure
-    model_save_path = './trained_models/simple_ae.pth'
     torch.save(model, model_save_path)
+
+def run_training(params: OrderedDict, save: bool = True):
+    runs = RunBuilder.get_runs(params)
+    folder = 'one_hidden_layer'
+
+    for run in runs:
+        model = AE_simple(run.latent)
+        model = model.to(device)
+
+        loss_fn = torch.nn.MSELoss()
+
+        optimizer = torch.optim.SGD(
+            model.parameters(), 
+            lr=config.learning_rate, 
+            momentum=0.9, 
+            weight_decay=config.l2_factor)
+
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+                optimizer,
+                mode="min",
+                factor=0.5,
+                patience=2,
+                verbose=True,
+                eps=1e-6,
+            )    
+
+        # Get datasets and form dataloaders
+        data_ = data.Data(kfold=False, balance_classes=config.balance_classes)
+        train_data, valid_data, test_data = data_.get_data(shuffle_sample_indices=True) 
+
+        save_test_dataset(test_data, folder, f'latent_{run.latent}')
+
+        train_dataset = data.ELMDataset(
+            *train_data,
+            config.signal_window_size,
+            config.label_look_ahead,
+            stack_elm_events=False,
+            transform=None,
+            for_autoencoder = True
+        )
+
+        valid_dataset = data.ELMDataset(
+            *valid_data,
+            config.signal_window_size,
+            config.label_look_ahead,
+            stack_elm_events=False,
+            transform=None,
+            for_autoencoder = True
+        )
+
+        batch_size = config.batch_size
+        train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        valid_dataloader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
+
+        epoch_avg_losses, all_losses = train(model, 
+            train_dataloader, 
+            valid_dataloader, 
+            optimizer, 
+            scheduler, 
+            loss_fn,
+            folder_name = folder)
+
+        # Save the model - weights and structure
+        if(save):
+            save_model(model, folder, f'simple_ae_latent_{run.latent}')
+
+if __name__ == '__main__':
+    params = OrderedDict(
+        latent = [500, 400]
+        )
+
+    run_training(params)
