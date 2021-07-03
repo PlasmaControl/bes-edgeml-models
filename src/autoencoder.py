9d6115733b857f64fec7264ac78146e4d2d1ae19
Prannav <parora9@wisc.edu>
2021-07-02 23:12:13 -0500

    added name to model, added debugging train function, added comments

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -26,7 +26,8 @@ class Autoencoder(torch.nn.Module):
         relu_negative_slope: float = 0.1,
         learning_rate: float = .0001,
         l2_factor: float = 5e-3,
-        dropout_rate: float = 0.3):
+        dropout_rate: float = 0.3,
+        name: str = None):
 
         super(Autoencoder, self).__init__()
 
@@ -48,6 +49,11 @@ class Autoencoder(torch.nn.Module):
         self.layers = self.create_layers()
 
         self.model = torch.nn.Sequential(self.layers)
+
+        if(name is None):
+            self.name = self._get_name()
+        else:
+            self.name = name
         
         return
 
@@ -94,15 +100,28 @@ class Autoencoder(torch.nn.Module):
     # Forward pass of the autoencoder - returns the reshaped output of net
     def forward(self, x):
         shape = x.shape
-        # print(shape)
+        # print(f'Before flatten: {x.shape}')
         x = self.flatten(x)
-        # print(x.shape)
+        # print(f'After flatten: {x.shape}')
 
         reconstructed = self.model(x)
         reconstructed = reconstructed.view(*shape)
         # print(reconstructed.shape)
 
         return reconstructed
+
+    def _get_name(self):
+        # n = type(self).__name__ + f'_{2 * len(self.encoder_hidden_layers) + 1}_hidden_{self.latent_dim}_latent'
+        s = type(self).__name__ + '_'
+        for i in self.encoder_hidden_layers:
+            s+= str(i) + '_'
+
+        s+= str(self.latent_dim) + '_'
+
+        for i in self.decoder_hidden_layers:
+            s+= str(i) + '_'
+
+        return s
 
 # Simple/easy Autoencoder class
 class AE_simple(torch.nn.Module):
@@ -150,10 +169,144 @@ class AE_simple(torch.nn.Module):
         # print(reconstructed.shape)
         return reconstructed
 
+# This train function is just for quick debugging - actual train function is in train_ae.py
+def train(model, 
+    train_dataloader: DataLoader, 
+    test_dataloader: DataLoader, 
+    optimizer,
+    scheduler, 
+    loss_fn, 
+    epochs: int = config.epochs,
+    print_output: bool = True):
+
+    epoch_avg_losses = []
+
+    for t in range(epochs):
+        if(print_output):
+            print(f"Epoch {t+1}\n-------------------------------")
+
+        train_loop(model, train_dataloader, optimizer, loss_fn)
+
+        epoch_avg_loss = validation_loop(model, test_dataloader, loss_fn)
+
+        epoch_avg_losses.append(epoch_avg_loss)
+        # tb.add_scalar('Epoch Avg Loss', epoch_avg_loss, t + 1)
+
+        # Change optimizer learning rate
+        scheduler.step(epoch_avg_loss)
+    
+    if(print_output):
+        print("Done Training!")
+
+    return epoch_avg_losses
+
+# Train loop for quick debugging    
+def train_loop(model, dataloader: DataLoader, optimizer, loss_fn, print_output: bool = True):
+    model.train()
+    size = len(dataloader.dataset) #Num sample windows in dataloader = batch_size * len(dataloader)
+    for batch, (X, y) in enumerate(dataloader):
+        # Compute prediction and loss
+        X = X.to(device)
+        y = y.to(device)
+        pred = model(X)
+        loss = loss_fn(pred, y) # Average loss for the given batch
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # For every 1000th batch:
+        if batch % 1000 == 0:
+            loss, current = loss.item(), batch * len(X) # len(X) is the batch size
+            if(print_output):
+                print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+# Validation loop for quick debugging        
+def validation_loop(model, dataloader: DataLoader, loss_fn, print_output: bool = True):
+    batches_in_dataloader = len(dataloader)
+
+    validation_loss = 0
+
+    model.eval()
+    
+    with torch.no_grad():
+        for X, y in dataloader:
+            X = X.to(device)
+            y = y.to(device)
+            pred = model(X)
+            cur_avg_batch_loss = loss_fn(pred, y).item()
+            validation_loss += cur_avg_batch_loss 
+
+    validation_loss /= batches_in_dataloader
+
+    if(print_output):
+        print(f"Validation Dataset Avg loss: {validation_loss:>8f} \n")
+    
+    return validation_loss
 
 if __name__ == '__main__':
-    model = Autoencoder(100, [400], [400])
-    summary(model)
+    model = Autoencoder(
+            300, 
+            [400], 
+            [400])
+
+    model = model.to(device)
+
+    loss_fn = torch.nn.MSELoss()
+
+    optimizer = torch.optim.SGD(
+        model.parameters(), 
+        lr=config.learning_rate, 
+        momentum=0.9, 
+        weight_decay=config.l2_factor)
+
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer,
+            mode="min",
+            factor=0.5,
+            patience=2,
+            verbose=True,
+            eps=1e-6,
+        )    
+
+    # Get datasets and form dataloaders
+    data_ = data.Data(kfold=False, balance_classes=config.balance_classes)
+    train_data, valid_data, test_data = data_.get_data(shuffle_sample_indices=True) 
+
+    train_dataset = data.ELMDataset(
+        *train_data,
+        config.signal_window_size,
+        config.label_look_ahead,
+        stack_elm_events=False,
+        transform=None,
+        for_autoencoder = True
+    )
+
+    valid_dataset = data.ELMDataset(
+        *valid_data,
+        config.signal_window_size,
+        config.label_look_ahead,
+        stack_elm_events=False,
+        transform=None,
+        for_autoencoder = True
+    )
+
+    batch_size = config.batch_size
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
+
+    epoch_avg_losses = train(model, 
+        train_dataloader, 
+        valid_dataloader, 
+        optimizer, 
+        scheduler, 
+        loss_fn)
+
+    plt.plot(epoch_avg_losses)
+    plt.show()
+
+    torch.save(model, './BEST_AE.pth')
 
 
     
