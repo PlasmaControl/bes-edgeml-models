d01483e90063eb1f258f48090a6f0530c8539aaa
Lakshya Malhotra <lm9679@traverse.princeton.edu>
2021-08-02 18:36:02 -0400

    Add changes in .

--- pt/config.py
+++ pt/config.py
@@ -28,7 +28,7 @@ max_elms = -1
 transition_halfwidth = 3
 
 # number of time data points to be used for a whole signal
-signal_window_size = 8
+signal_window_size = 16
 
 # size of the input after stitching and resizing
 size = 12
@@ -37,7 +37,7 @@ stack_elm_events = False
 # "look ahead", meaning the label for the entire signal window is taken to
 # be label corresponding to the last element (0 ahead) of the signal window;
 # (defaults to zero).
-label_look_ahead = 1200
+label_look_ahead = 0
 
 # Number of k-folds if using k-fold cross validation
 folds = 5
