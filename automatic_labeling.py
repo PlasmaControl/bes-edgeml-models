1c08687736041967aa92e76935dbf66895e3c743
LakshyaMalhotra <lakshya9009@gmail.com>
2021-08-12 22:55:18 -0700

    Add time gradients all channels and their differences.

--- automatic_labeling.py
+++ automatic_labeling.py
@@ -1,5 +1,4 @@
 import os
-import argparse
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -22,70 +21,64 @@ if __name__ == "__main__":
     start = window_start[0]
     stop = window_start[1] - 1
     first_signal = signals[start:stop]
+    signal_max = np.max(first_signal)
+    signal_min = np.min(first_signal)
+    print(f"Max, min values are respectively: {signal_max} and {signal_min}")
+    # first_signal = (first_signal - signal_min) / (signal_max - signal_min)
     first_label = labels[start:stop]
     print(first_signal.shape)
     print(first_label.shape)
     # print(first_signal[::8])
-    y1_2 = np.gradient(first_signal[::2], axis=0)
-    y2_2 = np.gradient(first_signal[::2], 2, axis=0)
-    y4_2 = np.gradient(first_signal[::2], 4, axis=0)
-    y8_2 = np.gradient(first_signal[::2], 8, axis=0)
 
     y1_4 = np.gradient(first_signal[::4], axis=0)
-    y2_4 = np.gradient(first_signal[::4], 2, axis=0)
-    y4_4 = np.gradient(first_signal[::4], 4, axis=0)
-    y8_4 = np.gradient(first_signal[::4], 8, axis=0)
-
-    y1_8 = np.gradient(first_signal[::8], axis=0)
-    y2_8 = np.gradient(first_signal[::8], 2, axis=0)
-    y4_8 = np.gradient(first_signal[::8], 4, axis=0)
-    y8_8 = np.gradient(first_signal[::8], 8, axis=0)
 
     # plt.plot(first_signal[::8, 0, 0], label='x')
-    fig, ax = plt.subplots(nrows=4, ncols=2, figsize=(12, 16))
-    ax = ax.flatten()
-    ax[0].plot(y1_2[:, 0, 0], label="y1_00, hop=2")
-    ax[0].plot(y2_2[:, 0, 0], label="y2_00, hop=2")
-    ax[0].plot(y4_2[:, 0, 0], label="y4_00, hop=2")
-    ax[0].plot(y8_2[:, 0, 0], label="y8_00, hop=2")
-    ax[0].legend()
+    fig, axs = plt.subplots(nrows=4, ncols=4, figsize=(18, 16))
+    axs = axs.flatten()
+    time_grad = y1_4.reshape(-1, 64)
+    print(f"Time gradient shape: {time_grad.shape}")
+    hop = 0
+    for ax in axs:
+        for i in range(4):
+            ax.plot(time_grad[:, i + hop], label=f"ch: {i+hop+1}")
+        ax.plot(first_label[::4], c="k", linestyle="--", label="ground truth")
+        ax.legend(fontsize=7, frameon=False)
+        hop += 4
+    plt.suptitle(
+        f"Time derivatives, hop-length: 4, signal window size: 128", fontsize=18
+    )
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+    if not args.dry_run:
+        plt.savefig(
+            os.path.join(
+                args.output_dir,
+                f"time_gradients_all_channels_hop_4_sws_128.png",
+            ),
+            dpi=100,
+        )
+    plt.show()
 
-    ax[1].plot(y1_2[:, -1, -1], label="y1_77, hop=2")
-    ax[1].plot(y2_2[:, -1, -1], label="y2_77, hop=2")
-    ax[1].plot(y4_2[:, -1, -1], label="y4_77, hop=2")
-    ax[1].plot(y8_2[:, -1, -1], label="y8_77, hop=2")
-    ax[1].legend()
-
-    ax[2].plot(y1_4[:, 0, 0], label="y1_00, hop=4")
-    ax[2].plot(y2_4[:, 0, 0], label="y2_00, hop=4")
-    ax[2].plot(y4_4[:, 0, 0], label="y4_00, hop=4")
-    ax[2].plot(y8_4[:, 0, 0], label="y8_00, hop=4")
-    ax[2].legend()
-
-    ax[3].plot(y1_4[:, -1, -1], label="y1_77, hop=4")
-    ax[3].plot(y2_4[:, -1, -1], label="y2_77, hop=4")
-    ax[3].plot(y4_4[:, -1, -1], label="y4_77, hop=4")
-    ax[3].plot(y8_4[:, -1, -1], label="y8_77, hop=4")
-    ax[3].legend()
-
-    ax[4].plot(y1_8[:, 0, 0], label="y1_00, hop=8")
-    ax[4].plot(y2_8[:, 0, 0], label="y2_00, hop=8")
-    ax[4].plot(y4_8[:, 0, 0], label="y4_00, hop=8")
-    ax[4].plot(y8_8[:, 0, 0], label="y8_00, hop=8")
-    ax[4].legend()
-
-    ax[5].plot(y1_8[:, -1, -1], label="y1_77, hop=8")
-    ax[5].plot(y2_8[:, -1, -1], label="y2_77, hop=8")
-    ax[5].plot(y4_8[:, -1, -1], label="y4_77, hop=8")
-    ax[5].plot(y8_8[:, -1, -1], label="y8_77, hop=8")
-    ax[5].legend()
-
-    ax[6].plot(first_signal[::2, 0, 0], label="signal_00, hop=2")
-    ax[6].plot(first_signal[::2, -1, -1], label="signal_77, hop=2")
-    ax[6].legend()
-
-    ax[7].plot(first_signal[::4, 0, 0], label="signal_00, hop=4")
-    ax[7].plot(first_signal[::4, -1, -1], label="signal_77, hop=4")
-    ax[7].legend()
-    plt.tight_layout()
+    time_grad_diffs = np.diff(time_grad, axis=0, prepend=0)
+    print(f"Time gradient diffs shape: {time_grad_diffs.shape}")
+    fig, axs = plt.subplots(nrows=4, ncols=4, figsize=(18, 16))
+    axs = axs.flatten()
+    hop = 0
+    for ax in axs:
+        for i in range(4):
+            ax.plot(time_grad_diffs[:, i + hop], label=f"ch: {i+hop+1}")
+        ax.plot(first_label[::4], c="k", linestyle="--", label="ground truth")
+        ax.legend(fontsize=7, frameon=False)
+        hop += 4
+    plt.suptitle(
+        f"Time derivatives' differences, hop-length: 4, signal window size: 128",
+        fontsize=18,
+    )
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+    if not args.dry_run:
+        plt.savefig(
+            os.path.join(
+                args.output_dir, f"time_gradients_diff_hop_4_sws_128.png"
+            ),
+            dpi=100,
+        )
     plt.show()
