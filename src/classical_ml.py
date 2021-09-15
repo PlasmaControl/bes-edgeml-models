4d48f3001f73de7dbb2f7d9f56704a5c87f048c0
LakshyaMalhotra <lakshya9009@gmail.com>
2021-09-15 16:22:08 -0700

    Run classical ml on extracted features.

--- src/classical_ml.py
+++ src/classical_ml.py
@@ -23,7 +23,7 @@ blues = list(sns.dark_palette("#69d", 8, reverse=True).as_hex())
 yellows = list(sns.color_palette("YlOrBr", 8).as_hex())
 
 if __name__ == "__main__":
-    lookahead = 0
+    lookahead = 200
     path = f"outputs/signal_window_16/label_look_ahead_{lookahead}/roc"
     plot_path = f"outputs/signal_window_16/label_look_ahead_{lookahead}/plots"
     # df = pd.read_csv(os.path.join(path, f"train_features_df_{lookahead}.csv"))
@@ -87,231 +87,231 @@ if __name__ == "__main__":
     cr_lr_df = pd.DataFrame(cr_lr).transpose()
     print(f"Classification report on logistic regression:\n{cr_lr_df}")
 
-    # random forest
-    print()
-    print()
-    rf = RandomForestClassifier(n_jobs=-1, random_state=23)
-    rf.fit(X_train, y_train)
-    y_train_rf = rf.predict_proba(X_train)
-    y_pred_rf = rf.predict_proba(X_valid)
-    y_pred_rf_bin = rf.predict(X_valid)
-    roc_score_train_rf = metrics.roc_auc_score(y_train, y_train_rf[:, 1])
-    roc_score_rf = metrics.roc_auc_score(y_valid, y_pred_rf[:, 1])
-    print(f"ROC score on training, random forest: {roc_score_train_rf}")
-    print(f"ROC score on validation, random forest: {roc_score_rf}")
+    # # random forest
+    # print()
+    # print()
+    # rf = RandomForestClassifier(n_jobs=-1, random_state=23)
+    # rf.fit(X_train, y_train)
+    # y_train_rf = rf.predict_proba(X_train)
+    # y_pred_rf = rf.predict_proba(X_valid)
+    # y_pred_rf_bin = rf.predict(X_valid)
+    # roc_score_train_rf = metrics.roc_auc_score(y_train, y_train_rf[:, 1])
+    # roc_score_rf = metrics.roc_auc_score(y_valid, y_pred_rf[:, 1])
+    # print(f"ROC score on training, random forest: {roc_score_train_rf}")
+    # print(f"ROC score on validation, random forest: {roc_score_rf}")
 
-    fpr, tpr, thresh = metrics.roc_curve(y_valid, y_pred_rf[:, 1])
-    roc_details_rf = pd.DataFrame()
-    roc_details_rf["fpr"] = fpr
-    roc_details_rf["tpr"] = tpr
-    roc_details_rf["threshold"] = thresh
+    # fpr, tpr, thresh = metrics.roc_curve(y_valid, y_pred_rf[:, 1])
+    # roc_details_rf = pd.DataFrame()
+    # roc_details_rf["fpr"] = fpr
+    # roc_details_rf["tpr"] = tpr
+    # roc_details_rf["threshold"] = thresh
 
-    roc_details_rf.to_csv(
-        os.path.join(
-            path,
-            f"roc_details_rf_{label_type}_lookahead_{lookahead}.csv",
-        ),
-        index=False,
-    )
-
-    cr_rf = metrics.classification_report(
-        y_valid, y_pred_rf_bin, output_dict=True
-    )
-    cr_rf_df = pd.DataFrame(cr_rf).transpose()
-    print(f"Classification report on random forest:\n{cr_rf_df}")
-    importances_rf = rf.feature_importances_
-    feature_importances_rf = pd.Series(importances_rf, index=features)
-
-    # random forest feature importance using feature permutation
-    # result = permutation_importance(
-    #     rf, X_valid, y_valid, n_repeats=6, random_state=23, n_jobs=-1
-    # )
-    # feature_importances_rf_fp = pd.Series(
-    #     result.importances_mean, index=features
+    # roc_details_rf.to_csv(
+    #     os.path.join(
+    #         path,
+    #         f"roc_details_rf_{label_type}_lookahead_{lookahead}.csv",
+    #     ),
+    #     index=False,
     # )
 
-    # XGBoost
-    print()
-    print()
-    xgb = XGBClassifier(
-        objective="binary:logistic",
-        use_label_encoder=False,
-        n_jobs=-1,
-        random_state=23,
-    )
-    xgb.fit(X_train, y_train, eval_metric="logloss")
-    y_train_xgb = xgb.predict_proba(X_train)
-    y_pred_xgb = xgb.predict_proba(X_valid)
-    y_pred_xgb_bin = xgb.predict(X_valid)
-    roc_score_train_xgb = metrics.roc_auc_score(y_train, y_train_xgb[:, 1])
-    roc_score_xgb = metrics.roc_auc_score(y_valid, y_pred_xgb[:, 1])
-    print(f"ROC score on training, XGBoost: {roc_score_train_xgb}")
-    print(f"ROC score on validation, XGBoost: {roc_score_xgb}")
+    # cr_rf = metrics.classification_report(
+    #     y_valid, y_pred_rf_bin, output_dict=True
+    # )
+    # cr_rf_df = pd.DataFrame(cr_rf).transpose()
+    # print(f"Classification report on random forest:\n{cr_rf_df}")
+    # importances_rf = rf.feature_importances_
+    # feature_importances_rf = pd.Series(importances_rf, index=features)
 
-    fpr, tpr, thresh = metrics.roc_curve(y_valid, y_pred_xgb[:, 1])
-    roc_details_xgb = pd.DataFrame()
-    roc_details_xgb["fpr"] = fpr
-    roc_details_xgb["tpr"] = tpr
-    roc_details_xgb["threshold"] = thresh
+    # # random forest feature importance using feature permutation
+    # # result = permutation_importance(
+    # #     rf, X_valid, y_valid, n_repeats=6, random_state=23, n_jobs=-1
+    # # )
+    # # feature_importances_rf_fp = pd.Series(
+    # #     result.importances_mean, index=features
+    # # )
 
-    roc_details_xgb.to_csv(
-        os.path.join(
-            path,
-            f"roc_details_xgb_{label_type}_lookahead_{lookahead}.csv",
-        ),
-        index=False,
-    )
+    # # XGBoost
+    # print()
+    # print()
+    # xgb = XGBClassifier(
+    #     objective="binary:logistic",
+    #     use_label_encoder=False,
+    #     n_jobs=-1,
+    #     random_state=23,
+    # )
+    # xgb.fit(X_train, y_train, eval_metric="logloss")
+    # y_train_xgb = xgb.predict_proba(X_train)
+    # y_pred_xgb = xgb.predict_proba(X_valid)
+    # y_pred_xgb_bin = xgb.predict(X_valid)
+    # roc_score_train_xgb = metrics.roc_auc_score(y_train, y_train_xgb[:, 1])
+    # roc_score_xgb = metrics.roc_auc_score(y_valid, y_pred_xgb[:, 1])
+    # print(f"ROC score on training, XGBoost: {roc_score_train_xgb}")
+    # print(f"ROC score on validation, XGBoost: {roc_score_xgb}")
 
-    cr_xgb = metrics.classification_report(
-        y_valid, y_pred_xgb_bin, output_dict=True
-    )
-    cr_xgb_df = pd.DataFrame(cr_xgb).transpose()
-    print(f"Classification report on XGBoost:\n{cr_xgb_df}")
-    importances_xgb = xgb.feature_importances_
-    feature_importances_xgb = pd.Series(importances_xgb, index=features)
-    valid_df.loc[:, "lr_pred"] = y_pred_lr[:, 1]
-    valid_df.loc[:, "rf_pred"] = y_pred_rf[:, 1]
-    valid_df.loc[:, "xgb_pred"] = y_pred_xgb[:, 1]
-    print(valid_df)
+    # fpr, tpr, thresh = metrics.roc_curve(y_valid, y_pred_xgb[:, 1])
+    # roc_details_xgb = pd.DataFrame()
+    # roc_details_xgb["fpr"] = fpr
+    # roc_details_xgb["tpr"] = tpr
+    # roc_details_xgb["threshold"] = thresh
 
-    # plotting
-    fig, ax = plt.subplots()
-    feature_importances_rf.plot(kind="bar", figsize=(16, 8), ax=ax)
-    ax.set_title(f"Feature importances using MDI, lookahead: {lookahead}")
-    ax.set_ylabel("Mean decrease in impurity")
-    fig.tight_layout()
-    plt.savefig(
-        os.path.join(
-            "outputs",
-            f"rf_feature_importance_mdi_{label_type}_lookahead_{lookahead}.png",
-        )
-    )
-    plt.show()
+    # roc_details_xgb.to_csv(
+    #     os.path.join(
+    #         path,
+    #         f"roc_details_xgb_{label_type}_lookahead_{lookahead}.csv",
+    #     ),
+    #     index=False,
+    # )
 
+    # cr_xgb = metrics.classification_report(
+    #     y_valid, y_pred_xgb_bin, output_dict=True
+    # )
+    # cr_xgb_df = pd.DataFrame(cr_xgb).transpose()
+    # print(f"Classification report on XGBoost:\n{cr_xgb_df}")
+    # importances_xgb = xgb.feature_importances_
+    # feature_importances_xgb = pd.Series(importances_xgb, index=features)
+    # valid_df.loc[:, "lr_pred"] = y_pred_lr[:, 1]
+    # valid_df.loc[:, "rf_pred"] = y_pred_rf[:, 1]
+    # valid_df.loc[:, "xgb_pred"] = y_pred_xgb[:, 1]
+    # print(valid_df)
+
+    # # plotting
     # fig, ax = plt.subplots()
-    # feature_importances_rf_fp.plot(kind="bar", figsize=(10, 8), ax=ax)
-    # ax.set_title("Feature importances using feature permutation")
-    # ax.set_ylabel("Mean accuracy decrease")
+    # feature_importances_rf.plot(kind="bar", figsize=(16, 8), ax=ax)
+    # ax.set_title(f"Feature importances using MDI, lookahead: {lookahead}")
+    # ax.set_ylabel("Mean decrease in impurity")
     # fig.tight_layout()
+    # plt.savefig(
+    #     os.path.join(
+    #         "outputs",
+    #         f"rf_feature_importance_mdi_{label_type}_lookahead_{lookahead}.png",
+    #     )
+    # )
     # plt.show()
 
-    fig, ax = plt.subplots()
-    feature_importances_xgb.plot(kind="bar", figsize=(16, 8), ax=ax)
-    ax.set_title(f"Feature importances, lookahead: {lookahead}")
-    fig.tight_layout()
-    plt.savefig(
-        os.path.join(
-            "outputs",
-            f"xgb_feature_importance_{label_type}_lookahead_{lookahead}.png",
-        )
-    )
-    plt.show()
+    # # fig, ax = plt.subplots()
+    # # feature_importances_rf_fp.plot(kind="bar", figsize=(10, 8), ax=ax)
+    # # ax.set_title("Feature importances using feature permutation")
+    # # ax.set_ylabel("Mean accuracy decrease")
+    # # fig.tight_layout()
+    # # plt.show()
 
-    # fpr/tpr vs threshold for lr
-    fig, ax = plt.subplots(figsize=(8, 6))
-    ax.plot(
-        roc_details_lr["threshold"].loc[1:],
-        roc_details_lr["fpr"].loc[1:],
-        "--",
-        lw=2,
-        c="#636EFA",
-        label="fpr lr",
-    )
-    ax.plot(
-        roc_details_lr["threshold"].loc[1:],
-        roc_details_lr["tpr"].loc[1:],
-        "-",
-        lw=2,
-        c="#636EFA",
-        label="tpr lr",
-    )
+    # fig, ax = plt.subplots()
+    # feature_importances_xgb.plot(kind="bar", figsize=(16, 8), ax=ax)
+    # ax.set_title(f"Feature importances, lookahead: {lookahead}")
+    # fig.tight_layout()
+    # plt.savefig(
+    #     os.path.join(
+    #         "outputs",
+    #         f"xgb_feature_importance_{label_type}_lookahead_{lookahead}.png",
+    #     )
+    # )
+    # plt.show()
 
-    # fpr/tpr vs threshold for rf
-    ax.plot(
-        roc_details_rf["threshold"].loc[1:],
-        roc_details_rf["fpr"].loc[1:],
-        "--",
-        lw=2,
-        c="#EF553B",
-        label="fpr rf",
-    )
-    ax.plot(
-        roc_details_rf["threshold"].loc[1:],
-        roc_details_rf["tpr"].loc[1:],
-        "-",
-        lw=2,
-        c="#EF553B",
-        label="tpr rf",
-    )
+    # # fpr/tpr vs threshold for lr
+    # fig, ax = plt.subplots(figsize=(8, 6))
+    # ax.plot(
+    #     roc_details_lr["threshold"].loc[1:],
+    #     roc_details_lr["fpr"].loc[1:],
+    #     "--",
+    #     lw=2,
+    #     c="#636EFA",
+    #     label="fpr lr",
+    # )
+    # ax.plot(
+    #     roc_details_lr["threshold"].loc[1:],
+    #     roc_details_lr["tpr"].loc[1:],
+    #     "-",
+    #     lw=2,
+    #     c="#636EFA",
+    #     label="tpr lr",
+    # )
 
-    # fpr/tpr vs threshold for xgb
-    ax.plot(
-        roc_details_xgb["threshold"].loc[1:],
-        roc_details_xgb["fpr"].loc[1:],
-        "--",
-        lw=2,
-        c="#00CC96",
-        label="fpr xgb",
-    )
-    ax.plot(
-        roc_details_xgb["threshold"].loc[1:],
-        roc_details_xgb["tpr"].loc[1:],
-        "-",
-        lw=2,
-        c="#00CC96",
-        label="tpr xgb",
-    )
-    ax.legend(fontsize=8, frameon=False)
-    ax.set_title(f"TPR/FPR vs threshold, lookahead: {lookahead}", fontsize=16)
-    ax.set_xlabel("threshold", fontsize=12)
-    plt.tight_layout()
-    plt.savefig(
-        os.path.join(
-            "outputs",
-            f"tpr_fpr_thresh_all_models_{label_type}_lookahead_{lookahead}.png",
-        )
-    )
-    plt.show()
+    # # fpr/tpr vs threshold for rf
+    # ax.plot(
+    #     roc_details_rf["threshold"].loc[1:],
+    #     roc_details_rf["fpr"].loc[1:],
+    #     "--",
+    #     lw=2,
+    #     c="#EF553B",
+    #     label="fpr rf",
+    # )
+    # ax.plot(
+    #     roc_details_rf["threshold"].loc[1:],
+    #     roc_details_rf["tpr"].loc[1:],
+    #     "-",
+    #     lw=2,
+    #     c="#EF553B",
+    #     label="tpr rf",
+    # )
 
-    # roc curves for different models
-    fig, ax = plt.subplots(figsize=(8, 6))
-    ax.plot(
-        roc_details_lr["fpr"].loc[1:],
-        roc_details_lr["tpr"].loc[1:],
-        "-",
-        lw=2,
-        c="#636EFA",
-        label="lr",
-    )
-    ax.plot(
-        roc_details_rf["fpr"],
-        roc_details_rf["tpr"],
-        "-",
-        lw=2,
-        c="#EF553B",
-        label="rf",
-    )
-    ax.plot(
-        roc_details_xgb["fpr"],
-        roc_details_xgb["tpr"],
-        "-",
-        lw=2,
-        c="#00CC96",
-        label="xgb",
-    )
-    ax.plot([0, 1], [0, 1], c="gray", lw=2)
-    ax.legend(fontsize=8, frameon=False)
-    ax.set_title(f"ROC Curve, lookahead: {lookahead}", fontsize=16)
-    ax.set_xlabel("FPR", fontsize=12)
-    ax.set_ylabel("TPR", fontsize=12)
-    plt.tight_layout()
-    plt.savefig(
-        os.path.join(
-            "outputs",
-            f"roc_curve_all_models_{label_type}_lookahead_{lookahead}.png",
-        )
-    )
-    plt.show()
+    # # fpr/tpr vs threshold for xgb
+    # ax.plot(
+    #     roc_details_xgb["threshold"].loc[1:],
+    #     roc_details_xgb["fpr"].loc[1:],
+    #     "--",
+    #     lw=2,
+    #     c="#00CC96",
+    #     label="fpr xgb",
+    # )
+    # ax.plot(
+    #     roc_details_xgb["threshold"].loc[1:],
+    #     roc_details_xgb["tpr"].loc[1:],
+    #     "-",
+    #     lw=2,
+    #     c="#00CC96",
+    #     label="tpr xgb",
+    # )
+    # ax.legend(fontsize=8, frameon=False)
+    # ax.set_title(f"TPR/FPR vs threshold, lookahead: {lookahead}", fontsize=16)
+    # ax.set_xlabel("threshold", fontsize=12)
+    # plt.tight_layout()
+    # plt.savefig(
+    #     os.path.join(
+    #         "outputs",
+    #         f"tpr_fpr_thresh_all_models_{label_type}_lookahead_{lookahead}.png",
+    #     )
+    # )
+    # plt.show()
+
+    # # roc curves for different models
+    # fig, ax = plt.subplots(figsize=(8, 6))
+    # ax.plot(
+    #     roc_details_lr["fpr"].loc[1:],
+    #     roc_details_lr["tpr"].loc[1:],
+    #     "-",
+    #     lw=2,
+    #     c="#636EFA",
+    #     label="lr",
+    # )
+    # ax.plot(
+    #     roc_details_rf["fpr"],
+    #     roc_details_rf["tpr"],
+    #     "-",
+    #     lw=2,
+    #     c="#EF553B",
+    #     label="rf",
+    # )
+    # ax.plot(
+    #     roc_details_xgb["fpr"],
+    #     roc_details_xgb["tpr"],
+    #     "-",
+    #     lw=2,
+    #     c="#00CC96",
+    #     label="xgb",
+    # )
+    # ax.plot([0, 1], [0, 1], c="gray", lw=2)
+    # ax.legend(fontsize=8, frameon=False)
+    # ax.set_title(f"ROC Curve, lookahead: {lookahead}", fontsize=16)
+    # ax.set_xlabel("FPR", fontsize=12)
+    # ax.set_ylabel("TPR", fontsize=12)
+    # plt.tight_layout()
+    # plt.savefig(
+    #     os.path.join(
+    #         "outputs",
+    #         f"roc_curve_all_models_{label_type}_lookahead_{lookahead}.png",
+    #     )
+    # )
+    # plt.show()
 
     # for elm_id in range(300, 315):
     #     first_elm_max_pool = valid_df[valid_df["elm_event"] == elm_id].loc[
--- src/roc_diff_models.py
+++ src/roc_diff_models.py
@@ -15,29 +15,29 @@ if __name__ == "__main__":
     path_100 = "outputs/signal_window_16/label_look_ahead_100/roc"
     path_150 = "outputs/signal_window_16/label_look_ahead_150/roc"
     path_200 = "outputs/signal_window_16/label_look_ahead_200/roc"
-    # path_400 = "outputs/signal_window_16/label_look_ahead_400/roc"
+    path_400 = "outputs/signal_window_16/label_look_ahead_400/roc"
 
     # # lr
-    # df_lr_0 = pd.read_csv(
-    #     os.path.join(path_0, "roc_details_lr_lookahead_0.csv")
-    # )
-    # df_lr_50 = pd.read_csv(
-    #     os.path.join(path_50, "roc_details_lr_lookahead_50.csv")
-    # )
-    # df_lr_100 = pd.read_csv(
-    #     os.path.join(path_100, "roc_details_lr_lookahead_100.csv")
-    # )
-    # df_lr_200 = pd.read_csv(
-    #     os.path.join(path_200, "roc_details_lr_lookahead_200.csv")
-    # )
+    df_lr_0 = pd.read_csv(
+        os.path.join(path_0, "roc_details_lr_manual_lookahead_0.csv")
+    )
+    df_lr_50 = pd.read_csv(
+        os.path.join(path_50, "roc_details_lr_manual_lookahead_50.csv")
+    )
+    df_lr_100 = pd.read_csv(
+        os.path.join(path_100, "roc_details_lr_manual_lookahead_100.csv")
+    )
+    df_lr_200 = pd.read_csv(
+        os.path.join(path_200, "roc_details_lr_manual_lookahead_200.csv")
+    )
     # df_lr_400 = pd.read_csv(
     #     os.path.join(path_400, "roc_details_lr_lookahead_400.csv")
     # )
 
     # # rf
-    # df_rf_0 = pd.read_csv(
-    #     os.path.join(path_0, "roc_details_rf_lookahead_0.csv")
-    # )
+    df_rf_0 = pd.read_csv(
+        os.path.join(path_0, "roc_details_rf_manual_lookahead_0.csv")
+    )
     # df_rf_50 = pd.read_csv(
     #     os.path.join(path_50, "roc_details_rf_lookahead_50.csv")
     # )
@@ -52,9 +52,9 @@ if __name__ == "__main__":
     # )
 
     # # xgb
-    # df_xgb_0 = pd.read_csv(
-    #     os.path.join(path_0, "roc_details_xgb_lookahead_0.csv")
-    # )
+    df_xgb_0 = pd.read_csv(
+        os.path.join(path_0, "roc_details_xgb_manual_lookahead_0.csv")
+    )
     # df_xgb_50 = pd.read_csv(
     #     os.path.join(path_50, "roc_details_xgb_lookahead_50.csv")
     # )
@@ -104,39 +104,39 @@ if __name__ == "__main__":
     )
 
     # # plotting lr
-    # fig, ax = plt.subplots(figsize=(8, 6))
-    # ax.plot(
-    #     df_lr_0["fpr"],
-    #     df_lr_0["tpr"],
-    #     "-",
-    #     lw=2,
-    #     # c="#636EFA",
-    #     label="lookahead: 0",
-    # )
-    # ax.plot(
-    #     df_lr_50["fpr"],
-    #     df_lr_50["tpr"],
-    #     "-",
-    #     lw=2,
-    #     # c="#EF553B",
-    #     label="lookahead: 50",
-    # )
-    # ax.plot(
-    #     df_lr_100["fpr"],
-    #     df_lr_100["tpr"],
-    #     "-",
-    #     lw=2,
-    #     # c="#00CC96",
-    #     label="lookahead: 100",
-    # )
-    # ax.plot(
-    #     df_lr_200["fpr"],
-    #     df_lr_200["tpr"],
-    #     "-",
-    #     lw=2,
-    #     # c="#00CC96",
-    #     label="lookahead: 200",
-    # )
+    fig, ax = plt.subplots(figsize=(6, 4), dpi=100)
+    ax.plot(
+        df_lr_0["fpr"],
+        df_lr_0["tpr"],
+        "-",
+        lw=2,
+        # c="#636EFA",
+        label="lookahead: 0",
+    )
+    ax.plot(
+        df_lr_50["fpr"],
+        df_lr_50["tpr"],
+        "-",
+        lw=2,
+        # c="#EF553B",
+        label="lookahead: 50",
+    )
+    ax.plot(
+        df_lr_100["fpr"],
+        df_lr_100["tpr"],
+        "-",
+        lw=2,
+        # c="#00CC96",
+        label="lookahead: 100",
+    )
+    ax.plot(
+        df_lr_200["fpr"],
+        df_lr_200["tpr"],
+        "-",
+        lw=2,
+        # c="#00CC96",
+        label="lookahead: 200",
+    )
     # ax.plot(
     #     df_lr_400["fpr"],
     #     df_lr_400["tpr"],
@@ -145,14 +145,18 @@ if __name__ == "__main__":
     #     # c="#00CC96",
     #     label="lookahead: 400",
     # )
-    # ax.plot([0, 1], [0, 1], c="gray", lw=2)
-    # ax.legend(fontsize=8, frameon=False)
-    # ax.set_xlabel("FPR", fontsize=12)
-    # ax.set_ylabel("TPR", fontsize=12)
-    # ax.set_title("Logistic Regression", fontsize=16)
-    # plt.tight_layout()
-    # plt.savefig(os.path.join("outputs", "lr_diff_lookaheads.png"))
-    # plt.show()
+    ax.plot([0, 1], [0, 1], c="gray", lw=2)
+    ax.legend(fontsize=9, frameon=False)
+    sns.despine(offset=10, trim=False)
+    ax.set_xlabel("FPR", fontsize=12)
+    ax.set_ylabel("TPR", fontsize=12)
+    ax.set_title("Logistic Regression", fontsize=16)
+    plt.gca().spines["left"].set_color("lightgrey")
+    plt.gca().spines["bottom"].set_color("lightgrey")
+    plt.grid(axis="y")
+    plt.tight_layout()
+    plt.savefig(os.path.join("outputs", "lr_diff_lookaheads.png"), dpi=100)
+    plt.show()
 
     # # plotting rf
     # fig, ax = plt.subplots(figsize=(8, 6))
@@ -317,9 +321,67 @@ if __name__ == "__main__":
     plt.gca().spines["bottom"].set_color("lightgrey")
     plt.grid(axis="y")
     plt.tight_layout()
-    plt.savefig(
-        os.path.join("outputs", "cnn_model_diff_lookaheads_wavelet.png"),
-        dpi=100,
-    )
+    # plt.savefig(
+    #     os.path.join("outputs", "cnn_model_diff_lookaheads_wavelet.png"),
+    #     dpi=100,
+    # )
     plt.show()
-#
+
+    # plotting all the models for lookahead 0
+    fig, ax = plt.subplots(figsize=(6, 4), dpi=100)
+    ax.plot(
+        df_lr_0["fpr"],
+        df_lr_0["tpr"],
+        "-",
+        lw=2,
+        # c="#636EFA",
+        label="lr",
+    )
+    ax.plot(
+        df_rf_0["fpr"],
+        df_rf_0["tpr"],
+        "-",
+        lw=2,
+        # c="#EF553B",
+        label="rf",
+    )
+    ax.plot(
+        df_xgb_0["fpr"],
+        df_xgb_0["tpr"],
+        "-",
+        lw=2,
+        # c="#00CC96",
+        label="xgb",
+    )
+    # ax.plot(
+    #     df_cnn_150["fpr"],
+    #     df_cnn_150["tpr"],
+    #     "-",
+    #     lw=2,
+    #     # c="#00CC96",
+    #     label="lookahead: 150",
+    # )
+    ax.plot(
+        df_cnn_0["fpr"],
+        df_cnn_0["tpr"],
+        "-",
+        lw=2,
+        # c="#00CC96",
+        label="cnn",
+    )
+    ax.plot([0, 1], [0, 1], c="gray", lw=2)
+    ax.legend(fontsize=10, frameon=False)
+    sns.despine(offset=10, trim=False)
+    ax.set_title("ROC curve", fontsize=13)
+    ax.set_xlabel("FPR", fontsize=13)
+    ax.set_ylabel("TPR", fontsize=13)
+    # ax.set_title("XGBoost", fontsize=16)
+    plt.gca().spines["left"].set_color("lightgrey")
+    plt.gca().spines["bottom"].set_color("lightgrey")
+    plt.grid(axis="y")
+    plt.tight_layout()
+    # plt.savefig(
+    #     os.path.join("outputs", "lr_rf_xgb_cnn_model_lookahead_0.png"),
+    #     dpi=100,
+    # )
+    plt.show()
