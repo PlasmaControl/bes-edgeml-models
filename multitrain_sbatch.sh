231216e39f8536d974e529a683268fc770510b21
LakshyaMalhotra <lakshya9009@gmail.com>
2021-10-18 17:30:49 -0700

    Add python path in slurm script.

--- multitrain_sbatch.sh
+++ multitrain_sbatch.sh
@@ -6,6 +6,7 @@
 #SBATCH --mem=128G
 #SBATCH --time=0-5
 #SBATCH --mail-type=begin
+#SBATCH --mail-type=fail
 #SBATCH --mail-type=end
 #SBATCH --mail-user=lmalhotra@wisc.edu
 #SBATCH --output=logs/slurm_out_%J.log
@@ -16,7 +17,7 @@ source "/scratch/gpfs/lm9679/miniconda3/etc/profile.d/conda.sh"
 
 conda activate torch
 
-echo "${which python}"
+echo $(which python)
 
 srun --exclusive -n 1 -c 4 python train.py --device cuda --model_name cnn --data_preproc wavelet --normalize_data --truncate_inputs --train_print_every 5000 --valid_print_every 1000 --n_epochs 40 --signal_window_size 8 --label_look_ahead 0 --num_workers 0 --lr 0.002 --weight_decay 0.05 --filename_suffix _wavelet &
 srun --exclusive -n 1 -c 4 python train.py --device cuda --model_name cnn --data_preproc wavelet --normalize_data --truncate_inputs --train_print_every 5000 --valid_print_every 1000 --n_epochs 40 --signal_window_size 8 --label_look_ahead 50 --num_workers 0 --lr 0.002 --weight_decay 0.05 --filename_suffix _wavelet &
