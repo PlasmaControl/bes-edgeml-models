98af181a0ac880c2d3e1e8dc3739d0f256149a75
LakshyaMalhotra <lakshya9009@gmail.com>
2021-10-20 11:01:09 -0400

    Run CNN model training for different sws and la.

--- multitrain_sbatch.sh
+++ multitrain_sbatch.sh
@@ -9,8 +9,8 @@
 #SBATCH --mail-type=fail
 #SBATCH --mail-type=end
 #SBATCH --mail-user=lmalhotra@wisc.edu
-#SBATCH --output=logs/slurm_out_%J.log
-#SBATCH --error=logs/slurm_err_%J.log
+#SBATCH --output=logs/slurm_out_%j-%2s.log
+#SBATCH --error=logs/slurm_err_%j-%2s.log
 module list
 
 source "/scratch/gpfs/lm9679/miniconda3/etc/profile.d/conda.sh"
@@ -19,9 +19,9 @@ conda activate torch
 
 echo $(which python)
 
-srun --exclusive -n 1 -c 4 python train.py --device cuda --model_name cnn --data_preproc wavelet --normalize_data --truncate_inputs --train_print_every 5000 --valid_print_every 1000 --n_epochs 40 --signal_window_size 8 --label_look_ahead 0 --num_workers 0 --lr 0.002 --weight_decay 0.05 --filename_suffix _wavelet &
-srun --exclusive -n 1 -c 4 python train.py --device cuda --model_name cnn --data_preproc wavelet --normalize_data --truncate_inputs --train_print_every 5000 --valid_print_every 1000 --n_epochs 40 --signal_window_size 8 --label_look_ahead 50 --num_workers 0 --lr 0.002 --weight_decay 0.05 --filename_suffix _wavelet &
-srun --exclusive -n 1 -c 4 python train.py --device cuda --model_name cnn --data_preproc wavelet --normalize_data --truncate_inputs --train_print_every 5000 --valid_print_every 1000 --n_epochs 40 --signal_window_size 8 --label_look_ahead 100 --num_workers 0 --lr 0.002 --weight_decay 0.05 --filename_suffix _wavelet &
-srun --exclusive -n 1 -c 4 python train.py --device cuda --model_name cnn --data_preproc wavelet --normalize_data --truncate_inputs --train_print_every 5000 --valid_print_every 1000 --n_epochs 40 --signal_window_size 8 --label_look_ahead 150 --num_workers 0 --lr 0.002 --weight_decay 0.05 --filename_suffix _wavelet &
-srun --exclusive -n 1 -c 4 python train.py --device cuda --model_name cnn --data_preproc wavelet --normalize_data --truncate_inputs --train_print_every 5000 --valid_print_every 1000 --n_epochs 40 --signal_window_size 8 --label_look_ahead 200 --num_workers 0 --lr 0.002 --weight_decay 0.05 --filename_suffix _wavelet &
+srun --exclusive -n 1 -c 4 python train.py --device cuda --model_name cnn --data_preproc wavelet --normalize_data --truncate_inputs --train_print_every 5000 --valid_print_every 1000 --n_epochs 35 --signal_window_size 32 --label_look_ahead 0 --num_workers 0 --lr 0.002 --weight_decay 0.05 --filename_suffix _wavelet &
+srun --exclusive -n 1 -c 4 python train.py --device cuda --model_name cnn --data_preproc wavelet --normalize_data --truncate_inputs --train_print_every 5000 --valid_print_every 1000 --n_epochs 25 --signal_window_size 32 --label_look_ahead 50 --num_workers 0 --lr 0.002 --weight_decay 0.05 --filename_suffix _wavelet &
+srun --exclusive -n 1 -c 4 python train.py --device cuda --model_name cnn --data_preproc wavelet --normalize_data --truncate_inputs --train_print_every 5000 --valid_print_every 1000 --n_epochs 25 --signal_window_size 32 --label_look_ahead 100 --num_workers 0 --lr 0.002 --weight_decay 0.05 --filename_suffix _wavelet &
+srun --exclusive -n 1 -c 4 python train.py --device cuda --model_name cnn --data_preproc wavelet --normalize_data --truncate_inputs --train_print_every 5000 --valid_print_every 1000 --n_epochs 25 --signal_window_size 32 --label_look_ahead 150 --num_workers 0 --lr 0.002 --weight_decay 0.05 --filename_suffix _wavelet &
+srun --exclusive -n 1 -c 4 python train.py --device cuda --model_name cnn --data_preproc wavelet --normalize_data --truncate_inputs --train_print_every 5000 --valid_print_every 1000 --n_epochs 25 --signal_window_size 32 --label_look_ahead 200 --num_workers 0 --lr 0.002 --weight_decay 0.05 --filename_suffix _wavelet &
 wait
---
+++ multitrain_sbatch_sws64.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --tasks=8
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:2
+#SBATCH --mem=128G
+#SBATCH --time=0-6
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=fail
+#SBATCH --mail-type=end
+#SBATCH --mail-user=lmalhotra@wisc.edu
+#SBATCH --output=logs/slurm_out_%j-%2s.log
+#SBATCH --error=logs/slurm_err_%j-%2s.log
+module list
+
+source "/scratch/gpfs/lm9679/miniconda3/etc/profile.d/conda.sh"
+
+conda activate torch
+
+echo $(which python)
+
+srun --exclusive -n 1 -c 4 python train.py --device cuda --model_name cnn --data_preproc wavelet --normalize_data --truncate_inputs --train_print_every 5000 --valid_print_every 1000 --n_epochs 35 --signal_window_size 64 --label_look_ahead 0 --num_workers 0 --lr 0.002 --weight_decay 0.05 --filename_suffix _wavelet &
+srun --exclusive -n 1 -c 4 python train.py --device cuda --model_name cnn --data_preproc wavelet --normalize_data --truncate_inputs --train_print_every 5000 --valid_print_every 1000 --n_epochs 25 --signal_window_size 64 --label_look_ahead 50 --num_workers 0 --lr 0.002 --weight_decay 0.05 --filename_suffix _wavelet &
+srun --exclusive -n 1 -c 4 python train.py --device cuda --model_name cnn --data_preproc wavelet --normalize_data --truncate_inputs --train_print_every 5000 --valid_print_every 1000 --n_epochs 25 --signal_window_size 64 --label_look_ahead 100 --num_workers 0 --lr 0.002 --weight_decay 0.05 --filename_suffix _wavelet &
+srun --exclusive -n 1 -c 4 python train.py --device cuda --model_name cnn --data_preproc wavelet --normalize_data --truncate_inputs --train_print_every 5000 --valid_print_every 1000 --n_epochs 25 --signal_window_size 64 --label_look_ahead 150 --num_workers 0 --lr 0.002 --weight_decay 0.05 --filename_suffix _wavelet &
+srun --exclusive -n 1 -c 4 python train.py --device cuda --model_name cnn --data_preproc wavelet --normalize_data --truncate_inputs --train_print_every 5000 --valid_print_every 1000 --n_epochs 25 --signal_window_size 64 --label_look_ahead 200 --num_workers 0 --lr 0.002 --weight_decay 0.05 --filename_suffix _wavelet &
+wait
