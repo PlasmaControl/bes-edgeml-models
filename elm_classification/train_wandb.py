97b7678b6f4d287feb792121090cd969d98d69ae
LakshyaMalhotra <lakshya9009@gmail.com>
2021-11-09 11:43:21 -0800

    Add date in the wandb project name.

--- elm_classification/train_wandb.py
+++ elm_classification/train_wandb.py
@@ -163,7 +163,7 @@ def train_loop(
     #     )
     #     fold = None
     with wandb.init(
-        project=f"{args.model_name}_sws_{args.signal_window_size}",
+        project=f"{args.model_name}_sws_{args.signal_window_size}_{time.strftime('%m%d%Y')}",
         config=config,
     ):
         wandb.run.name = f"lookahead_{args.label_look_ahead}"
