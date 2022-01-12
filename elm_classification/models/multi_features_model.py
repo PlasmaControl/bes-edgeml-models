a3ee2e4aac93d659af8f0bf55ea7cce23c32dcbb
LakshyaMalhotra <lakshya9009@gmail.com>
2022-01-12 09:53:16 -0800

    Add scales in a geometric progression upto 1024.

--- elm_classification/models/multi_features_model.py
+++ elm_classification/models/multi_features_model.py
@@ -146,13 +146,7 @@ class CWTFeatureModel(nn.Module):
         """
         super(CWTFeatureModel, self).__init__()
         self.args = args
-        # filter_size = (
-        #     (self.args.signal_window_size, 8, 8)
-        #     if self.args.signal_window_size <= 64
-        #     else (int(self.args.signal_window_size / 2), 8, 8)
-        # )
         filter_size = (int(np.log2(1024)) + 1, 8, 8)
-        # filter_size = (52, 8, 8)
         self.conv = nn.Conv3d(
             in_channels=1, out_channels=num_filters, kernel_size=filter_size
         )
