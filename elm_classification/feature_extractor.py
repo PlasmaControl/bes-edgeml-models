31ab8a2a8a8936987935e899ee7b17b106c5b1f9
LakshyaMalhotra <lakshya9009@gmail.com>
2022-02-11 17:16:48 -0800

    Add docstrings for the different functions.

--- elm_classification/feature_extractor.py
+++ elm_classification/feature_extractor.py
@@ -1,4 +1,11 @@
-import argparse
+"""Script to extract features from the convolutional neural network. These
+features can be used as input features for classical machine learning models
+like logistic regression, random forest etc. For instance, we took the features
+out of the 3D convolution layer from the `FeatureModel`.
+
+See here for more details:
+https://pytorch.org/tutorials/beginner/former_torchies/nnft_tutorial.html#forward-and-backward-function-hooks
+"""
 import os
 
 import numpy as np
@@ -12,6 +19,15 @@ activation = dict()
 
 
 def get_activation(name):
+    """
+    Get the activation out of the given layer from a neural network.
+
+    Args:
+        name (str): Name of the layer.
+
+    Returns:
+        Callable.
+    """
     def hook(model, input, output):
         activation[name] = output.detach()
 
@@ -19,6 +35,18 @@ def get_activation(name):
 
 
 def elm_feature_df(elm_indices: np.ndarray, all_data: tuple):
+    """
+    Create a pandas dataframe where the feature map from the forward hook can be
+    stored.
+
+    Args:
+        elm_indices (np.ndarray): NumPy array containing the ELM IDs.
+        all_data (tuple): Tuple containing the signals, labels, valid_indices
+        and window start indices.
+
+    Returns:
+        pd.DataFrame: Dataframe containing the feature map for each ELM event.
+    """
     signals, labels, valid_indices, window_start = all_data
 
     print(f"Signals shape: {signals.shape}")
@@ -48,9 +76,12 @@ if __name__ == "__main__":
     model_ckpt_dir = "model_checkpoints/signal_window_16"
     args, parser = TestArguments().parse(verbose=True)
     utils.test_args_compat(args, parser, infer_mode=True)
+
+    # load the output paths
     paths = utils.create_output_paths(args, infer_mode=True)
     roc_dir = paths[-1]
 
+    # create the logger object and instantiate the model
     LOGGER = utils.get_logger(script_name=__name__)
     model_cls = utils.create_model(args.model_name)
     model = model_cls(args)
@@ -58,9 +89,10 @@ if __name__ == "__main__":
     model = model.to(device)
     print(model)
 
+    # load the saved model
     model_ckpt_path = os.path.join(
         model_ckpt_dir,
-        f"{args.model_name}_lookahead_{args.label_look_ahead}_truncate.pth",
+        f"{args.model_name}_lookahead_{args.label_look_ahead}_{args.data_preproc}{args.filename_suffix}.pth",
     )
     print(f"Using elm_model checkpoint: {model_ckpt_path}")
     model.load_state_dict(
@@ -69,20 +101,27 @@ if __name__ == "__main__":
             map_location=device,
         )["model"]
     )
-    model.pool.register_forward_hook(get_activation("pool"))
+    # register the model layer with the forward hooks
+    model.conv.register_forward_hook(get_activation("conv"))
 
+    # create data object from data preprocessing pipeline
     data_cls = utils.create_data(args.data_preproc)
     data_obj = data_cls(args, LOGGER)
 
+    # load all the ELM events (need to use `--use_all_data` command line arg in
+    # order to do so)
     all_elms, all_data = data_obj.get_data()
     signals, labels, valid_indices, window_start = all_data
     df = elm_feature_df(all_elms, all_data)
 
+    # create PyTorch dataset from all data
     data = dataset.ELMDataset(
         args, signals, labels, valid_indices, window_start, LOGGER
     )
     cnn_feature_map = []
     look_ahead_labels = []
+
+    # iterate through all the rows of the dataframe
     for i in range(len(df)):
         i_prev = 0 if i == 0 else i - 1
         if df.loc[i, "elm_id"] != df.loc[i_prev, "elm_id"]:
@@ -92,22 +131,27 @@ if __name__ == "__main__":
         input_signals, input_labels = data.__getitem__(i)
         input_signals = input_signals.unsqueeze(0)
         model_out = model(input_signals)
-        feature_map = activation["pool"]
+        feature_map = activation["conv"]
         feature_map = torch.flatten(feature_map, start_dim=0)
         cnn_feature_map.append(feature_map.numpy().tolist())
         look_ahead_labels.append(input_labels.numpy().tolist())
-    df["cnn_feature_map"] = cnn_feature_map
+    df[f"{args.model_name}_feature_map"] = cnn_feature_map
     df["label"] = look_ahead_labels
+
+    # clean the dataframe to have one column per feature
     expanded_feature_df = pd.DataFrame(
         df["cnn_feature_map"].to_list(),
         columns=[f"f_{i+1}" for i in range(len(cnn_feature_map[0]))],
         dtype=np.float32,
     )
     print(expanded_feature_df.info(memory_usage="deep"))
-    df.drop(["cnn_feature_map"], axis=1, inplace=True)
+    df.drop([f"{args.model_name}_feature_map"], axis=1, inplace=True)
     df = pd.concat([df, expanded_feature_df], axis=1)
     print(df)
+
+    # save the dataframe as a CSV file (not very memory efficient, could use HDF5.
+    # Pandas support HDF5 format).
     df.to_csv(
-        os.path.join(roc_dir, f"cnn_feature_df_{args.label_look_ahead}.csv"),
+        os.path.join(roc_dir, f"{args.model_name}_feature_df_{args.label_look_ahead}.csv"),
         index=False,
     )
