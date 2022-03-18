a94ee1f3a9102403453657d74b5cb379d8e851c4
David R. Smith <drsmith8@wisc.edu>
2022-03-18 13:25:23 -0400

    mods

--- test/test_elm_prediction.py
+++ test/test_elm_prediction.py
@@ -1,10 +1,12 @@
 import sys
 import pytest
+import shutil
 
 from elm_prediction.train import train_loop
 from elm_prediction.analyze import do_analysis
 
 
+RUN_DIR = 'run_dir'
 DEFAULT_INPUT_ARGS = {
     'max_elms':10,
     'n_epochs':1,
@@ -15,47 +17,48 @@ DEFAULT_INPUT_ARGS = {
 
 def test_train_loop_raw_only():
     input_args = DEFAULT_INPUT_ARGS.copy()
-    input_args['output_dir'] = 'run_dir/raw_only'
+    input_args['output_dir'] = RUN_DIR + '/raw_only'
     train_loop(input_args)
 
 def test_train_loop_save_onnx():
     input_args = DEFAULT_INPUT_ARGS.copy()
-    input_args['output_dir'] = 'run_dir/save_onnx'
+    input_args['output_dir'] = RUN_DIR + '/save_onnx'
     input_args['save_onnx'] = True
     train_loop(input_args)
 
 def test_train_loop_list_args():
     input_args = [f"--{key}={value}" for key, value in DEFAULT_INPUT_ARGS.items()]
-    input_args.extend(['--output_dir', 'run_dir/list_args'])
+    input_args.extend(['--output_dir', RUN_DIR + '/list_args'])
     train_loop(input_args)
 
-def test_train_loop_fft():
+def test_train_loop_with_fft():
     input_args = DEFAULT_INPUT_ARGS.copy()
-    input_args['output_dir'] = 'run_dir/fft'
+    input_args['output_dir'] = RUN_DIR + '/with_fft'
     input_args['fft_num_filters'] = 8
     train_loop(input_args)
 
-def test_train_loop_dwt():
+def test_train_loop_with_dwt():
     input_args = DEFAULT_INPUT_ARGS.copy()
-    input_args['output_dir'] = 'run_dir/dwt'
+    input_args['output_dir'] = RUN_DIR + '/with_dwt'
     input_args['dwt_num_filters'] = 8
     train_loop(input_args)
 
-def test_train_loop_dct():
+def test_train_loop_with_dct():
     input_args = DEFAULT_INPUT_ARGS.copy()
-    input_args['output_dir'] = 'run_dir/dct'
+    input_args['output_dir'] = RUN_DIR + '/with_dct'
     input_args['dct_num_filters'] = 8
     train_loop(input_args)
 
 def test_analyze():
-    args_file = 'run_dir/raw_only/args.pkl'
+    args_file = RUN_DIR + '/raw_only/args.pkl'
     do_analysis(args_file, interactive=False, click_through_pages=False, save=True)
 
 
 if __name__=="__main__":
-    command_line_args = [
-        '-v',
+    shutil.rmtree(RUN_DIR, ignore_errors=True)
+    pytest_args = [
+        '--verbose',
     ]
     sys.exit(
-        pytest.main(command_line_args)
+        pytest.main(pytest_args)
     )
